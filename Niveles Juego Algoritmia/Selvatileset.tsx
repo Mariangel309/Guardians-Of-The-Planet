ce0c1597668204ebb35a7ec33bc96d608e7db387
martin-s-24 <mserrano2425@gmail.com>
2025-05-12 20:27:18 -0500

    mapa nivel 1

--- Niveles Juego Algoritmia/Selvatileset.tsx
+++ Niveles Juego Algoritmia/Selvatileset.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="Selvatileset" tilewidth="16" tileheight="16" tilecount="72" columns="9">
- <image source="../Downloads/theJungleTileSet.png" width="159" height="132"/>
+ <image source="theJungleTileSet.png" width="159" height="132"/>
  <tile id="10">
   <objectgroup>
    <object id="1" x="0" y="0" width="16" height="16"/>
