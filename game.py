068bdc94642f842623a03e599a918f3f5401a29b
martin-s-24 <mserrano2425@gmail.com>
2025-05-14 17:06:21 -0500

    Ya está el menú, assets de basura y corazones

--- game.py
+++ game.py
@@ -4,6 +4,18 @@ import pygame
 from scripts.utils import load_image, load_images
 from scripts.entities import PhysicsEntity
 
+class Button:
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+
+    def draw(self, surface):
+        pass # no dibuja nada
+    def is_clicked(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                return True
+        return False
+
 class Game:
     def __init__(self):
         pygame.init() #Función que inicializa todos los módulos de Pygame que necesitan ser activados antes de usarlos
@@ -27,7 +39,46 @@ class Game:
 
         self.player = PhysicsEntity(self, 'player', (50, 50), (16, 26))
 
+    def main_menu(self):
+        background = pygame.image.load('data/images/background main menu/background1.png').convert()
+        background = pygame.transform.scale(background, (640, 480))  # Ajustar al tamaño de la ventana
+
+        # Crear botones con texto más pequeño
+        play_button = Button(48, 420, 132, 30)
+        store_button = Button(194, 420, 132, 30)
+        credits_button = Button(340, 420, 132, 30)
+        quit_button = Button(486, 420, 120, 30)
+
+        while True:
+            self.screen.blit(background, (0, 0))  # Fondo del menú
+
+
+            # Dibujar botones
+            play_button.draw(self.screen)
+            store_button.draw(self.screen)
+            credits_button.draw(self.screen)
+            quit_button.draw(self.screen)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+                if play_button.is_clicked(event):
+                    return  # Iniciar juego
+                if store_button.is_clicked(event):
+                    print("Abrir tienda")
+                if credits_button.is_clicked(event):
+                    print("Mostrar créditos")
+                if quit_button.is_clicked(event):
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.update()
+            self.clock.tick(60)
+
     def run(self):
+        self.main_menu()  # Llamamos al menú principal antes de iniciar el juego
         while True: #Este es un bucle infinito que se repite una y otra vez, esto para actualizar y dibujar todo
             self.screen.fill((14, 219, 248))
 
