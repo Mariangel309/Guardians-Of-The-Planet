f0dea70b7dccf409c184d63a079ac7b6cb49061b
Maria Angel <mariaangelm309@gmail.com>
2025-05-15 08:41:29 -0500

    Sólidos implementados, gravedad y saltos

--- game.py
+++ game.py
@@ -1,57 +1,62 @@
 import sys
+
 import pygame
 
 from scripts.utils import load_image, load_images
 from scripts.entities import PhysicsEntity
+from scripts.tilemap import Tilemap
 
 class Game:
     def __init__(self):
-        pygame.init() #Función que inicializa todos los módulos de Pygame que necesitan ser activados antes de usarlos
+        pygame.init()
 
-        pygame.display.set_caption('Ninja Game')
-        #pygame.display es el módulo que controla la pantalla o ventana principal del juego
-
-        self.screen = pygame.display.set_mode((640, 480)) #Creación de la ventana del juego con dimensiones (ancho, alto)
-
-        self.clock = pygame.time.Clock() #Esta es una herramienta que se usa para controlar la velocidad del juego, i.e. cuantas veces por segundo se actualiza la pantalla (frames per second FPS)
+        pygame.display.set_caption('Guardians of The Planet')
+        self.screen = pygame.display.set_mode((640, 480))
+        self.display = pygame.Surface((320, 240))
+        self.clock = pygame.time.Clock()
         
         self.movement = [False, False]
-
+        
         self.assets = {
-            'decor': load_images('tiles/decor'), 
             'decor': load_images('tiles/decor'),
-            'decor': load_images('tiles/decor'),
-            'decor': load_images('tiles/decor'),
+            'grass': load_images('tiles/grass'),
+            'large_decor': load_images('tiles/large_decor'),
+            'stone': load_images('tiles/stone'),
             'player': load_image('entities/player.png')
         }
-
-        self.player = PhysicsEntity(self, 'player', (50, 50), (16, 26))
-
+        
+        self.player = PhysicsEntity(self, 'player', (50, 130), (14, 23))
+        
+        self.tilemap = Tilemap(self, tile_size=16)
+        
     def run(self):
-        while True: #Este es un bucle infinito que se repite una y otra vez, esto para actualizar y dibujar todo
-            self.screen.fill((14, 219, 248))
-
-            self.player.update((self.movement[1] - self.movement[0], 0))
-            self.player.render(self.screen)
-
-
-            for event in pygame.event.get(): #Detecta las entradas del usuario
+        while True:
+            self.display.fill((14, 219, 248))
+            
+            self.tilemap.render(self.display)
+            
+            self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
+            self.player.render(self.display)
+            
+            for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
-                    sys.exit() #Cierra la app cuando el usuario presione la X de cerrar
+                    sys.exit()
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LEFT:
                         self.movement[0] = True
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
-
+                    if event.key == pygame.K_UP:
+                        self.player.velocity[1] = -3
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
                         self.movement[0] = False
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
             
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
             pygame.display.update()
-            self.clock.tick(60)  #60 repeticiones por segundo
+            self.clock.tick(60)
 
 Game().run()
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,4 +1,5 @@
 import os
+
 import pygame
 
 BASE_IMG_PATH = 'data/images/'
@@ -8,8 +9,9 @@ def load_image(path):
     img.set_colorkey((0, 0, 0))
     return img
 
+#Funcion que carga todos los tiles
 def load_images(path):
     images = []
-    for img_name in os.listdir(BASE_IMG_PATH + path):
+    for img_name in sorted(os.listdir(BASE_IMG_PATH + path)):
         images.append(load_image(path + '/' + img_name))
     return images
