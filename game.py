7a99b1f357ea6117d87d173469f99e4e8ce65650
martin-s-24 <mserrano2425@gmail.com>
2025-05-17 21:14:02 -0500

    menu listo

--- game.py
+++ game.py
@@ -11,19 +11,25 @@ class Button:
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
 
-    def draw(self, surface):
-        pass
-
     def is_clicked(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
             if self.rect.collidepoint(event.pos):
                 return True
         return False
 
+    def draw(self, surface):
+        pygame.draw.rect(surface, (255, 0, 0), self.rect, 2)
+
 
 class Game:
     def __init__(self):
         pygame.init()
+
+
+        self.volume = 0.5  # volumen inicial (50%)
+        pygame.mixer.init()
+        pygame.mixer.music.set_volume(self.volume)
+
 
         pygame.display.set_caption('Ninja Game')
         self.screen = pygame.display.set_mode((640, 480))
@@ -31,8 +37,9 @@ class Game:
 
         self.clock = pygame.time.Clock()
 
-        self.movement = [False, False]
+        self.font = pygame.font.Font('data/Fonts/static/Oswald-Medium.ttf', 40)
 
+        self.movement = [False, False]
 
         self.assets = {
             'decor': load_images('tiles/decor'),
@@ -44,30 +51,70 @@ class Game:
             'clouds': load_images('clouds')
         }
 
+        self.menu_assets = {
+            'background': pygame.image.load('data/images/menu_backgrounds/primero.png'),
+            'titulo': pygame.image.load('data/images/menu_backgrounds/titulo.png'),
+            'boton_jugar': pygame.image.load('data/images/menu_backgrounds/jugar.png'),
+            'boton_tienda': pygame.image.load('data/images/menu_backgrounds/tienda.png'),
+            'boton_creditos': pygame.image.load('data/images/menu_backgrounds/creditos.png'),
+            'boton_salir': pygame.image.load('data/images/menu_backgrounds/salir.png'),
+            'icono_config': pygame.image.load('data/images/menu_backgrounds/configuracion.png'),
+            'signo_mas': pygame.transform.scale(pygame.image.load('data/images/menu_backgrounds/mas.png'), (70, 70)),
+            'volumen_titulo': pygame.transform.scale(pygame.image.load('data/images/menu_backgrounds/volumen.png'), (420, 370)),
+            'signo_menos': pygame.transform.scale(pygame.image.load('data/images/menu_backgrounds/menos.png'), (70, 70))
+        
+        }        
+        # Escalamos las imagenes del menú
+        self.menu_assets['background'] = pygame.transform.smoothscale(self.menu_assets['background'], (640, 480))        
+        self.menu_assets['titulo'] = pygame.transform.scale(self.menu_assets['titulo'], (600, 430))
+        self.menu_assets['boton_jugar'] = pygame.transform.scale(self.menu_assets['boton_jugar'], (132, 70))
+        self.menu_assets['boton_tienda'] = pygame.transform.scale(self.menu_assets['boton_tienda'], (132, 70))
+        self.menu_assets['boton_creditos'] = pygame.transform.scale(self.menu_assets['boton_creditos'], (132, 70))
+        self.menu_assets['boton_salir'] = pygame.transform.scale(self.menu_assets['boton_salir'], (120, 70))
+        self.menu_assets['icono_config'] = pygame.transform.scale(self.menu_assets['icono_config'], (80, 70))
+
         self.clouds = Clouds(self.assets['clouds'], count=16)
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
         self.tilemap = Tilemap(self, tile_size=16)
 
-
-        #Camara para el videojuego
+        # Camara para el videojuego
         self.scroll = [0, 0]
 
     def main_menu(self):
-        background = pygame.image.load('data/images/background main menu/background1.png').convert()
-        background = pygame.transform.scale(background, (640, 480))
 
-        play_button = Button(48, 420, 132, 30)
-        store_button = Button(194, 420, 132, 30)
-        credits_button = Button(340, 420, 132, 30)
-        quit_button = Button(486, 420, 120, 30)
+        pygame.mixer.music.load('data/music.wav')  # Ruta a el archivo de música
+        pygame.mixer.music.play(-1)  # -1 significa que se reproduce en bucle infinito
+
+        background = self.menu_assets['background'] 
+        ancho = 640
+
+        # Botones con su propio rectángulo (posición + tamaño independiente de la imagen)
+        play_button = Button(73, 402, 110, 27)
+        store_button = Button(220, 402, 110, 27)
+        credits_button = Button(367, 402, 119, 27)
+        quit_button = Button(525, 403, 90, 27)
+        icono_configuracion = Button(568, 32, 47, 47)
+
+        i = 0  # Variable para el movimiento del fondo
 
         while True:
-            self.screen.blit(background, (0, 0))
+            i -= 1  # mueve el fondo 1 píxel hacia la izquierda cada frame
+            if i <= -ancho:
+                i = 0  # reinicia el loop
 
-            play_button.draw(self.screen)
-            store_button.draw(self.screen)
-            credits_button.draw(self.screen)
-            quit_button.draw(self.screen)
+            # Dibuja dos fondos seguidos para loop continuo
+            self.screen.blit(background, (i, 0))
+            self.screen.blit(background, (i + ancho, 0))
+
+            self.screen.blit(self.menu_assets['titulo'], (20, 10))  # Se pone el título en la posición correcta
+
+            # Se ponen las imagenes de los botones en la posición correcta
+            self.screen.blit(self.menu_assets['boton_jugar'], (60, 380))
+            self.screen.blit(self.menu_assets['boton_tienda'], (210, 380))
+            self.screen.blit(self.menu_assets['boton_creditos'], (360, 380))
+            self.screen.blit(self.menu_assets['boton_salir'], (510, 380))
+            self.screen.blit(self.menu_assets['icono_config'], (550, 20))
+
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -75,7 +122,8 @@ class Game:
                     sys.exit()
 
                 if play_button.is_clicked(event):
-                    return  # Salir del menú e iniciar el juego
+                    pygame.mixer.music.stop()
+                    return
                 if store_button.is_clicked(event):
                     print("Abrir tienda")
                 if credits_button.is_clicked(event):
@@ -83,15 +131,56 @@ class Game:
                 if quit_button.is_clicked(event):
                     pygame.quit()
                     sys.exit()
+                if icono_configuracion.is_clicked(event):
+                    self.configuracion_menu()
 
             pygame.display.update()
             self.clock.tick(60)
 
+    def configuracion_menu(self):
+
+        volver_img = pygame.transform.scale(pygame.image.load("data/images/menu_backgrounds/volveralmenu.png"), (300, 200))
+        volver = Button(500, 400, 100, 40)
+        aumentar = Button(390, 205, 52, 60)
+        disminuir = Button(190, 220, 52, 30)
+        volver = Button(195, 310, 250, 80)
+
+        while True:
+            self.screen.blit(self.menu_assets['background'], (0, 0))
+
+            self.screen.blit(self.menu_assets['volumen_titulo'], (110, -100))  # Posición del título de volumen
+            
+            # Dibujar imagen de volver al menú
+            self.screen.blit(volver_img, (170, 250)) 
+
+            self.screen.blit(self.menu_assets['signo_mas'], (380, 200))
+            self.screen.blit(self.menu_assets['signo_menos'], (180, 200))
+            
+            # Mostrar el volumen como porcentaje
+            volumen_texto = self.font.render(f'{int(self.volume * 100)}%', True, (240, 180, 35))
+            self.screen.blit(volumen_texto, (282, 210))
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if aumentar.is_clicked(event):
+                    self.volume = min(1.0, self.volume + 0.1)
+                    pygame.mixer.music.set_volume(self.volume)
+                if disminuir.is_clicked(event):
+                    self.volume = max(0.0, self.volume - 0.1)
+                    pygame.mixer.music.set_volume(self.volume)
+                if volver.is_clicked(event):
+                    return
+
+            pygame.display.update()
+            self.clock.tick(60)
     def run(self):
+
         while True:
             self.display.blit(self.assets['background'], (0, 0))
 
-            #Movimiento de la cámara directamente al jugador
+            # Movimiento de la cámara directamente al jugador
 
             self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
             self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
@@ -131,4 +220,3 @@ if __name__ == '__main__':
     game = Game()
     game.main_menu()  # Mostrar menú antes de iniciar el juego
     game.run()
-
